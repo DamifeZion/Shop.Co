3ce6282d18ca90225e7975a3c0ff4ab9aec04e3e
DamifeZion <damifemartins@gmail.com>
2023-08-22 15:19:27 +0100

    initial commit

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,18 +1,18 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import { Home, ProductDetail, Category } from "./pages/exportPages";
 
 function App() {
-
   return (
     <BrowserRouter>
       <Routes>
-        <Route />
-        <Route />
-        <Route />
-        <Route />
+        <Route path="/" element={<Home />} />
+        <Route path="/product-detail" element={<ProductDetail />} />
+        <Route path="/categories" element={<Category />} />
+        <Route path="*" element={"404 Not Found"} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ web/src/pages/exportPages.js
@@ -0,0 +1,6 @@
+export {default as Home} from './Home' 
+
+export {default as ProductDetail} from './ProductDetail'
+
+export {default as Category} from './Category'
+
